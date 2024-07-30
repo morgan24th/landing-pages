1aa12ff58171669a806a2d49217ccf26d6ddf1d6
morgan24th <sarpong.prince28@gmail.com>
2024-07-30 17:54:55 -0400

    captalized question page

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-export default function page() {
+export default function QuestionsPage() {
   const [randomQuestions, setRandomQuestions] = useState<quiz_questions[]>([]);
   // types
   interface quiz_questions {
